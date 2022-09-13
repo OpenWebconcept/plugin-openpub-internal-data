d185f2b37c22d15e0ee09b3730c61f7d925e49f4
niekh1234 <niek.hagen@yard.nl>
2022-09-13 11:39:30 +0200

    (chore): rewording

--- config/metaboxes.php
+++ config/metaboxes.php
@@ -1,9 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 return [
     'internaldata' => [
         'id'         => 'openpub_internaldata',
-        'title'      => __('Internal Data', 'openpub-internal-products'),
+        'title'      => __('Internal Data', 'openpub-internal-data'),
         'post_types' => ['openpub-item'],
         'context'    => 'normal',
         'priority'   => 'high',
@@ -18,18 +20,18 @@ return [
                     'type'       => 'group',
                     'clone'      => true,
                     'sort_clone' => true,
-                    'add_button' => __('Add internal data', 'openpub-internal-products'),
+                    'add_button' => __('Add internal data', 'openpub-internal-data'),
                     'fields'     => [
                         [
                             'id'   => 'internaldata_key',
-                            'name' => __('Title', 'openpub-internal-products'),
+                            'name' => __('Title', 'openpub-internal-data'),
                             'type' => 'text',
                         ],
                         [
                             'id'      => 'internaldata_value',
-                            'name'    => __('Content', 'openpub-internal-products'),
+                            'name'    => __('Content', 'openpub-internal-data'),
                             'type'    => 'wysiwyg',
-                            'desc'    => __('Use of HTML is allowed', 'openpub-internal-products'),
+                            'desc'    => __('Use of HTML is allowed', 'openpub-internal-data'),
                             'options' => [
                                 'textarea_rows' => 4,
                                 'teeny'         => false,
