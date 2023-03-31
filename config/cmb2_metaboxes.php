6e3a1ffd7b88538ed890ea37adfd656a50959ab4
Niek Hagen <niekhagen@hotmail.com>
2023-03-31 15:24:52 +0200

    (feat): cmb2

---
+++ config/cmb2_metaboxes.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'internaldata' => [
+        'id'           => 'openpub_internaldata',
+        'title'        => __('Internal Data', 'openpub-internal-data'),
+        'object_types' => ['openpub-item'],
+        'context'      => 'normal',
+        'priority'     => 'high',
+        'autosave'     => true,
+        'fields'       => [
+            'internaldata' => [
+                'id'         => 'openpub_internaldata',
+                'type'       => 'group',
+                'heading'    => [
+                    'type' => 'heading',
+                    'name' => __('Internal Data', 'openpub-internal-data'),
+                ],
+                'internaldata' => [
+                    'id'         => 'openpub_internaldata_group',
+                    'type'       => 'group',
+                    'options'    => [
+                        'add_button'        => __('Add internal data', 'openpub-internal-data'),
+                        'remove_button'     => __('Remove internal data', 'openpub-internal-data'),
+                    ],
+                    'fields'     => [
+                        [
+                            'id'   => 'internaldata_key',
+                            'name' => __('Title', 'openpub-internal-data'),
+                            'type' => 'text',
+                        ],
+                        [
+                            'id'      => 'internaldata_value',
+                            'name'    => __('Content', 'openpub-internal-data'),
+                            'type'    => 'wysiwyg',
+                            'desc'    => __('Use of HTML is allowed', 'openpub-internal-data'),
+                            'options' => [
+                                'textarea_rows' => 4,
+                                'teeny'         => false,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
--- config/core.php
+++ config/core.php
@@ -37,7 +37,7 @@ return [
         [
             'type'    => 'plugin',
             'label'   => 'OpenPub Base',
-            'version' => '2.0.0',
+            'version' => '3.0.0',
             'file'    => 'openpub-base/openpub-base.php',
         ],
     ],
